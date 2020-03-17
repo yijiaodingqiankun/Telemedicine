5b9d75cf248695b083accd4ee5cdcb914054ef57
sinduanan <565166941@qq.com>
2020-03-17 17:21:06 +0800

    1721

--- app/src/main/java/com/hanmi/telemedicine/fragment/MineFragment.kt
+++ app/src/main/java/com/hanmi/telemedicine/fragment/MineFragment.kt
@@ -4,19 +4,22 @@ import android.view.Gravity
 import android.view.View
 import android.widget.TextView
 import com.hanmi.telemedicine.common.BaseFragment
+import com.hanmi.telemedicine.databinding.Mine04LayoutBinding
 
 class MineFragment : BaseFragment() {
+    lateinit var binding: Mine04LayoutBinding
     override fun beforeView() {
-        // load binding
+        binding = Mine04LayoutBinding.inflate(layoutInflater)
     }
 
     override fun initView(): View? {
-        val textView = TextView(mContext).apply {
-            text = ""
-            textSize = 35f
-            gravity = Gravity.CENTER
-        }
-        return textView
+//        val textView = TextView(mContext).apply {
+//            text = ""
+//            textSize = 35f
+//            gravity = Gravity.CENTER
+//        }
+//        return textView
+        return binding.root
     }
 
 }
